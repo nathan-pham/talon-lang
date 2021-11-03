f2d6370550d362cab093563edcb2b232a0276975
Nathan Pham <nathanpham.me@gmail.com>
2021-11-03 14:24:17 -0700

    tokens

---
+++ token.py
@@ -0,0 +1,23 @@
+ILLEGAL = "ILLEGAL"
+EOF = "EOF"
+
+# identifiers and literals
+IDENT = "IDENT"
+INT = "INT"
+
+# operations
+ASSIGN = "="
+PLUS = "+"
+
+# delimiters
+COMMA = ","
+SEMICOLON = ";"
+
+LPAREN = "("
+RPAREN = ")"
+LBRACE = "{"
+RBRACE = "}"
+
+# keywords
+FUNCTION = "FUNCTION"
+LET = "LET"
