3857d4a56caf3a4ba9c780933a9c5ad768467d53
Nathan Pham <nathanpham.me@gmail.com>
2021-11-05 12:13:13 -0700

    create integer literal

--- language/ast.py
+++ language/ast.py
@@ -1,3 +1,4 @@
+# Talon Program class
 class Program:
 
     statements = []
@@ -8,6 +9,7 @@ class Program:
         else:
             return None
 
+# Node class
 class Node:
     def token_literal(self):
         return self.token.literal
@@ -15,10 +17,12 @@ class Node:
     def __str__(self):
         return self.token_literal()
 
+# Node -> Statement class
 class Statement(Node):
     def statement_node(self):
         pass
 
+# Node -> Expression class
 class Expression(Node):
     value = None
     token = None
@@ -26,16 +30,22 @@ class Expression(Node):
     def expression_node(self):
         pass
 
+    def __str__(self):
+        return self.token_literal()
+
+# Node -> Statement -> ExpressionStatement class
 class ExpressionStatement(Statement):
     def __init__(self, token, expression=None):
         self.token = token
         self.expression = expression
 
+# Node -> Statement -> ReturnStatement class
 class ReturnStatement(Statement):
     def __init__(self, token, return_value=None):
         self.token = token
         self.return_value = return_value
 
+# Node -> Statement -> LetStatement class
 class LetStatement(Statement):
     def __init__(self, token, name=None, value=None):
         self.token = token
@@ -45,7 +55,17 @@ class LetStatement(Statement):
     def statement_node(self):
         pass
 
+# Node -> Expression -> Identifier class
 class Identifier(Expression):
     def __init__(self, token, value):
+        self.token = token
+        self.value = value
+
+# Node -> Expression -> IntegerLiteral class
+class IntegerLiteral(Expression):
+    token = None
+    value = None
+
+    def __init__(self, token, value=None):
         self.token = token
         self.value = value
