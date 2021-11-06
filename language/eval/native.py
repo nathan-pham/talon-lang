520fe914a0db6e2bd3c4da21ecb10d0b0a83a3f6
Nathan Pham <nathanpham.me@gmail.com>
2021-11-06 08:26:34 -0700

    basic type checking

--- language/eval/native.py
+++ language/eval/native.py
@@ -1,13 +1,66 @@
 import language.eval.Object as Object
 
+TRUE = Object.Boolean(True)
+FALSE = Object.Boolean(False)
+NULL = Object.Null()
+
 def len_(*args):
-    if len(args) != 1: return Object.Error(f"wrong number of arguments: 1 required but {len(args)} given") 
+    error = argument_error(args, 1)
+    if error: return error
 
     if isinstance(args[0], Object.String): return Object.Number(len(args[0].value))
     if isinstance(args[0], Object.Array): return Object.Number(len(args[0].elements))
 
     return Object.Error(f"argument to 'len' not supported, got: {args[0].type_}")
 
+def puts(*args):
+    for arg in args: print(arg.inspect())
+    return NULL
+
+def str_(*args):
+    error = argument_error(args, 1)
+    if error: return error
+
+    return Object.String(str(args[0]))
+
+def int_(*args):
+    error = argument_error(args, 1)
+    if error: return error
+
+    if isinstance(args[0], Object.Number): return Object.Number(int(args[0].value))
+    if isinstance(args[0], Object.String): return Object.Number(int(args[0].value))
+
+    return Object.Error(f"argument to 'int' not supported, got: {args[0].type_}")
+
+def float_(*args):
+    error = argument_error(args, 1)
+    if error: return error
+
+    if isinstance(args[0], Object.Number): return Object.Number(float(args[0].value))
+    if isinstance(args[0], Object.String): return Object.Number(float(args[0].value))
+
+    return Object.Error(f"argument to 'float' not supported, got: {args[0].type_}")
+
+def type_(*args):
+    error = argument_error(args, 1)
+    if error: return error
+
+    return Object.String(args[0].type_)
+
+def argument_error(args, required):
+    if len(args) != required:
+        return Object.Error(f"wrong number of arguments: {required} required but {len(args)} given")
+
 native_functions = {
-    "len": Object.Native(len_)
+    # length of objects
+    "len": Object.Native(len_),
+
+    # output to display
+    "puts": Object.Native(puts),
+
+    # loose type checking
+    "str": Object.Native(str_),
+    "int": Object.Native(int_),
+    "float": Object.Native(float_),
+    "type": Object.Native(type_),
 }
